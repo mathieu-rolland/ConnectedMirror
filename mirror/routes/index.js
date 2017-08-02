5d41599ac72a20293e0ec3a3aa84a1a3bfee3fe7
Mathieu <mathieu.rolland2705@gmail.com>
2017-08-02 22:32:08 +0200

    Display weather on /weather

--- mirror/routes/index.js
+++ mirror/routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var weatherController = require('../controllers/WeatherController');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/weather' , weatherController.getActualWeather );
 
 module.exports = router;
